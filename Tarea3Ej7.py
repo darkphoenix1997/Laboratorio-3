6c5c640074c7c50cbaec731fb69b6935121cf3ba
Luis Manuel García Valdivia <darkfenix.lmgv@gmail.com>
2016-12-13 16:23:01 -0600

    Add files via upload

--- Tarea3Ej7.py
+++ Tarea3Ej7.py
@@ -3,20 +3,42 @@
 
 # In[ ]:
 
-#7
-nombre= input("Nombre")
-nombre2= name
-vocal= "aeiouAEIOU"
+print "\nPrograma que recibe nombre y da el verso a cantar\n"
 
-if nombre2[0] in vocal:
-  nombre2=nombre
-else:
-  nombre=nombre2.replace(nombre2[0])
+def nombresc(a): 
+	b=''
+	for i in range(0,len(a)-1):
+		b=b+a[i+1]
+	return b
 
-if nombre[0]=="b" or nombre[0]=="B" or nombre[0]=="f" or nombre[0]=="F" or nombre[0]=="m" or nombre[0]=="M":
-  print (nombre!, nombre, "bo", "b", nombre2, "Bannana Fanna fo F", nombre2, "Fe Fi Mo M", nombre2, nombre)
+def evaluarc(a):
+	j=0
+	while j==0:
+		if (a[0]=='a' or a[0]=='e' or a[0]=='i' or a[0]=='o' or a[0]=='u'):
+			pass
+			j=1
+		else:
+			a=nombresc(a)
+	return a
 
-else:
-  print (nombre, nombre, "bo", "b", nombre2, "Bannana Fanna fo F", nombre2, "Fe Fi Mo M", nombre2, nombre)
-#Luis Manuel Garcia Valdivia
+def verso(a):
+	print a,a
+	if (a[0]=='a' or a[0]=='e' or a[0]=='i' or a[0]=='o' or a[0]=='u'):
+		print "bob"+a
+	else:
+		print "bob"+evaluarc(a)
+	print "banana bana"
+	if (a[0]=='a' or a[0]=='e' or a[0]=='i' or a[0]=='o' or a[0]=='u'):
+		print "fof"+a
+	else:
+		print "fof"+evaluarc(a)
+	print "fi fai mo"
+	if (a[0]=='a' or a[0]=='e' or a[0]=='i' or a[0]=='o' or a[0]=='u'):
+		print "m"+a
+	else:
+		print "m"+evaluarc(a)
+	print a
+	
+nombre=raw_input("Ingrese un nombre corto: ")
+verso(nombre)
 
